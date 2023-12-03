5437b53f7e3cc32c05cc8323fb73c2861212b277
FaresQaddoura29 <faresqaddoura@gmail.com>
2023-12-03 17:54:10 +0000

    added more community routes

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const Community = require("../models/Community.model")
 const {url} = require("../config.js")
 
+//     /community
+
 
 router.get("/all", async (req, res)=>{
     try{
@@ -15,6 +17,8 @@ router.get("/all", async (req, res)=>{
         res.status(402).json({msg: "Could not get communities data from server"})
     }
 })
+
+
 router.post("/all", async (req, res)=>{
     try{
         const response = await Community.create(req.body)
@@ -25,5 +29,35 @@ router.post("/all", async (req, res)=>{
         res.status(402).json({msg: "Could not add community. Server Error"})
     }
 })
-
+router.get("/find/:id", async (req, res)=>{
+    try{
+        const response = await Community.findById(req.params.id)
+        .populate("admin", "-password -friendRequests")
+        .populate("members", "-password")
+        res.status(201).json(response)
+    }
+    catch(err){
+        res.status(402).json({msg: "Could not get community data from server"})
+    }
+})
+router.put("/edit/:id", async (req, res)=>{
+    try{
+        const response = await Community.findByIdAndUpdate(req.params.id, req.body)
+        console.log(response);
+        res.status(201).json(response)
+    }
+    catch(err){
+        res.status(402).json({msg: "Could not edit community, error from server"})
+    }
+})
+router.delete("/delete/:id", async (req, res)=>{
+    try{
+        const response = await Community.findByIdAndDelete(req.params.id)
+        console.log(response);
+        res.status(201).json(response)
+    }
+    catch(err){
+        res.status(402).json({msg: "Could not delete community, error from server"})
+    }
+})
 module.exports = router
